668fa95972d1ea42f18089daa27ad0d18a46ab1e
anamaksimova <19cherrytree91@gmail.com>
2021-03-30 20:14:17 +0600

    j3lesson7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,58 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Main {
+    public static void main(String[] args) {
+         Class test = MyTest.class;
+        try {
+            start(test);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+   public static void start(Class testClass) throws InvocationTargetException, IllegalAccessException {
+       int i=0; int j=0;
+       ArrayList<Method> tests = new ArrayList<>();
+       Method[] methods = testClass.getDeclaredMethods();
+       for (Method m : methods) {
+           if (m.isAnnotationPresent(BeforeSuite.class)) {
+              i++;
+               if (i>1) {
+                   throw new RuntimeException("аннотации @BeforeSuite должны присутствовать" +
+                           " в единственном экземпляре");}
+               else  {
+                   System.out.println(m.getAnnotation(BeforeSuite.class).description());
+                   m.invoke(null);
+               }
+           }
+
+           if (m.isAnnotationPresent(AfterSuite.class)) {
+               j++;
+               if (j > 1) {
+                   throw new RuntimeException("аннотации @AfterSuite должны присутствовать" +
+                           " в единственном экземпляре");
+               }
+           }
+
+           if (m.isAnnotationPresent(Test.class)) {
+           tests.add(m);
+          }
+       }
+       tests.sort(Comparator.comparingInt(x -> x.getAnnotation(Test.class).priority()));
+       for (Method testsTest : tests) {
+           System.out.println(testsTest.getAnnotation(Test.class).description());
+           testsTest.invoke(null);
+       }
+       for (Method n : methods) {
+           if (n.isAnnotationPresent(AfterSuite.class)) {
+               n.invoke(null);
+               System.out.println(n.getAnnotation(AfterSuite.class).description());
+           }
+       }
+   }
 }
